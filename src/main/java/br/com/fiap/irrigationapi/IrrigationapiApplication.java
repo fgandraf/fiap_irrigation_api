c15dac9869daac7ee32edc0a4e2c624e54b31c14
Henry Thuler <henrythuler2003@outlook.com>
2024-05-23 19:31:11 -0300

    project structure

---
+++ src/main/java/br/com/fiap/irrigationapi/IrrigationapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.irrigationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrrigationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/irrigationapi/IrrigationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.irrigationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrrigationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
